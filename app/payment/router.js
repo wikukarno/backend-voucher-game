9782896a255e71e6b8f06f3157b7d3bda8599059
wikukarno <prasetyagama2@gmail.com>
2023-05-20 23:20:56 +0700

    Add CRUD for Payment

---
+++ app/payment/router.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var router = express.Router();
+const {
+  index,
+  viewCreate,
+  actionCreate,
+  viewEdit,
+  actionEdit,
+  actionDelete,
+//   actionStatus,
+} = require("./controller");
+
+router.get("/", index);
+router.get("/create", viewCreate);
+router.post('/create', actionCreate);
+router.get('/edit/:id', viewEdit);
+router.put('/edit/:id', actionEdit);
+router.delete('/delete/:id', actionDelete);
+// router.put('/status/:id', actionStatus);
+
+module.exports = router;
