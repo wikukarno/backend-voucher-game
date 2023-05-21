866b2d0777541346bd1b0329adcdd38b07eeefb4
wikukarno <prasetyagama2@gmail.com>
2023-05-21 11:43:14 +0700

    Add Sign In and Add Middleware Every Page

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const nominalRouter = require("./app/nominal/router");
 const voucherRouter = require("./app/voucher/router");
 const bankRouter = require("./app/bank/router");
 const paymentRouter = require("./app/payment/router");
+const usersRouter = require("./app/users/router");
 
 var app = express();
 
@@ -38,7 +39,8 @@ app.use(
   express.static(path.join(__dirname, "/public/adminlte/")),
 );
 
-app.use("/", dashboardRouter);
+app.use("/", usersRouter);
+app.use("/dashboard", dashboardRouter);
 app.use("/category", categoryRouter);
 app.use("/nominal", nominalRouter);
 app.use("/voucher", voucherRouter);
--- app/payment/router.js
+++ app/payment/router.js
@@ -10,6 +10,9 @@ const {
 //   actionStatus,
 } = require("./controller");
 
+const { isLoginAdmin } = require("../middleware/auth");
+
+router.use(isLoginAdmin);
 router.get("/", index);
 router.get("/create", viewCreate);
 router.post('/create', actionCreate);
